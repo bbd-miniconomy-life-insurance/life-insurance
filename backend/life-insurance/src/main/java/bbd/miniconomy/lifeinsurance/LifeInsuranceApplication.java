7f39961829716e3d3412266abd81adac39932c86
cameron <cameron.zwane@bbd.co.za>
2024-06-28 20:07:40 +0200

    removed retry library

--- backend/life-insurance/src/main/java/bbd/miniconomy/lifeinsurance/LifeInsuranceApplication.java
+++ backend/life-insurance/src/main/java/bbd/miniconomy/lifeinsurance/LifeInsuranceApplication.java
@@ -2,10 +2,8 @@ package bbd.miniconomy.lifeinsurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableRetry
 public class LifeInsuranceApplication {
 
 	public static void main(String[] args) {
