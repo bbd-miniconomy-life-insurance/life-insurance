dbf9fea9e7be1264e164d8c7662db5cf22197357
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-06-30 14:18:46 +0200

    Some stocks stuff

---
+++ backend/life-insurance/src/main/java/bbd/miniconomy/lifeinsurance/services/InternalService.java
@@ -0,0 +1,41 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.util.List;
+
+@Service
+public class InternalService {
+    private final PolicyRepository policyRepository;
+    private final PriceRepository priceRepository;
+    private final double probabilityOfDeathInMonth = 0.1;
+
+    @Autowired
+    public InternalService(PolicyRepository policyRepository, PriceRepository priceRepository) {
+        this.policyRepository = policyRepository;
+        this.priceRepository = priceRepository;
+    }
+
+    @Transactional
+    public void Taxes() {
+        // Not sure how this works yet
+    }
+
+    @Transactional
+    public void BuyStocksWithAvailableMoney() {
+        double moneyForStocks = EstimatedMonthlyIncome() * 0.3;
+
+        // Buy stocks
+    }
+
+    private double EstimatedMonthlyIncome(){
+        long activatePoliciesCount = policyRepository.countActivePolicies();
+        double currentPremiumPrice = priceRepository.findFirstByOrderByInceptionDateDesc();
+        
+        double moneyIn = activatePoliciesCount * currentPremiumPrice;
+        double moneyOut = activatePoliciesCount * currentPremiumPrice * 30 * probabilityOfDeathInMonth;
+
+        return Math.max(0, moneyIn - moneyOut);
+    }
+}
