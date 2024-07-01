6630a2557d88052b8b062d281cb828fee842e75f
MitchdeKock <159277915+MitchdeKock@users.noreply.github.com>
2024-07-01 19:42:02 +0200

    Added transaction and debit order tables

--- database/V202406231847__CreateSchema.sql
+++ database/V202406231847__CreateSchema.sql
@@ -5,10 +5,21 @@ CREATE TABLE "policy_status" (
 
 CREATE TABLE "policy" (
   "policy_id" SERIAL PRIMARY KEY,
-  "persona_id" BIGINT UNIQUE NOT NULL,
-  "status_id" INT NOT NULL,
-  "inception_date" TIMESTAMP NOT NULL,
-  FOREIGN KEY ("status_id") REFERENCES "policy_status" ("status_id")
+  "persona_id" bigint NOT NULL,
+  "status_id" bigint NOT NULL,
+  "inception_date" TIMESTAMP NOT NULL
+);
+
+CREATE TABLE "debit_order" (
+  "debit_order_id" SERIAL PRIMARY KEY,
+  "policy_id" bigint NOT NULL,
+  "debit_order_reference_number" string NOT NULL
+);
+
+CREATE TABLE "transaction" (
+  "transaction_id" SERIAL PRIMARY KEY,
+  "policy_id" bigint NOT NULL,
+  "transaction_reference_number" string NOT NULL
 );
 
 CREATE TABLE "payment_history" (
@@ -25,3 +36,8 @@ CREATE TABLE "price" (
   "price" NUMERIC(12,2) NOT NULL
 );
 
+ALTER TABLE "policy" ADD FOREIGN KEY ("status_id") REFERENCES "policy_status" ("status_id");
+
+ALTER TABLE "transaction" ADD FOREIGN KEY ("policy_id") REFERENCES "policy" ("policy_id");
+
+ALTER TABLE "debit_order" ADD FOREIGN KEY ("policy_id") REFERENCES "policy" ("policy_id");
