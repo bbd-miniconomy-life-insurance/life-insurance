cc9a3c184335eb9b5827eaf9c61d017b8cb91425
cameron <cameron.zwane@bbd.co.za>
2024-07-04 09:15:50 +0200

    updates

--- backend/life-insurance/src/main/java/bbd/miniconomy/lifeinsurance/configs/WebConfiguration.java
+++ backend/life-insurance/src/main/java/bbd/miniconomy/lifeinsurance/configs/WebConfiguration.java
@@ -9,11 +9,7 @@ public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods("*")
-                .allowedOrigins("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        registry.addMapping("/**").allowedOrigins("*");
     }
 }
 
